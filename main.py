cfcd6d7b2b45271271c47822111d78c422cb0251
ewuk <ewukwork@gmail.com>
2026-04-09 00:51:38 +0300

    6

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 from src.ImagePreprocessor import ImagePreprocessor
+import torch
 
 if __name__ == "__main__":
-    img_path = "data/girls/best/3.jfif"
+    img_path = "data/girls/best/9.jpg"
     ip = ImagePreprocessor()
     # ip.without_background2()
     # ip.without_background(img_path)
-    ip.refine_mask_with_depth(img_path)
+    print("CUDA available:", torch.cuda.is_available())
+    print("Torch CUDA version:", torch.version.cuda)
+    print("Device count:", torch.cuda.device_count())
+    # ip.refine_mask_with_depth(img_path)
